9cc6ab72f048f7d3066a83c458fcd0ebc3488605
euro <phuchit@omise.co>
2019-07-23 19:16:22 +0700

    Setup project structure

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+YellowBox.ignoreWarnings([
+  'Warning: componentWillMount',
+  'Warning: componentWillReceiveProps',
+  'Module RCTImageLoader',
+  'Class RCTCxxModule was not exported',
+  'Remote debugger'
+]);
+
 AppRegistry.registerComponent(appName, () => App);
