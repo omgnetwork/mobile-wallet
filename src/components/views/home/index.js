b2b77c7e8830e2b7bd169ba24a1151d1e1edf8d7
euro <phuchit@omise.co>
2019-07-24 15:02:06 +0700

    Add dependencies `react-navigation`, `react-navigation-material-bottom-tabs`, `react-native-paper`, `react-native-vector-icons`

---
+++ src/components/views/home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { Button } from 'react-native-paper'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+export default Home
