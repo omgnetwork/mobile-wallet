f9d01da6d34dffa395a2983e8efd838be92b8751
euro <phuchit@omise.co>
2019-07-31 13:38:19 +0700

    Try new widgets on preview

--- src/components/views/preview/index.js
+++ src/components/views/preview/index.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import { View } from 'react-native'
 import { withNavigation } from 'react-navigation'
+import { Surface, withTheme } from 'react-native-paper'
+import { OMGRadioButton, OMGTextInput } from '../../widgets'
 
-const Preview = ({ navigation }) => {
-  return <View style={{ flexDirection: 'column' }} />
+const Preview = ({ navigation, theme }) => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        paddingHorizontal: 16,
+        backgroundColor: theme.colors.background
+      }}>
+      <OMGRadioButton
+        choices={['Keystore', 'Mnemonic', 'Private Key', 'Just 4th Button']}
+        onSelected={selectedIndex => {
+          console.log(`selected: ${selectedIndex}`)
+        }}
+      />
+      <OMGTextInput
+        title='Name'
+        inputs={[
+          { placeholder: 'Wallet', secured: false },
+          { placeholder: 'Password', secured: true },
+          { placeholder: 'Confirm Password', secured: true }
+        ]}
+      />
+    </View>
+  )
 }
 
-export default withNavigation(Preview)
+export default withNavigation(withTheme(Preview))
