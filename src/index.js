763b8aeaf6f52dfbf6f466f462dad0b678fd4488
euro <phuchit@omise.co>
2019-07-31 13:37:08 +0700

    Add theme

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React, { Fragment, useState, useEffect } from 'react'
-import { SafeAreaView, StatusBar, StyleSheet } from 'react-native'
+import { StatusBar, StyleSheet, View } from 'react-native'
+import { SafeAreaView } from 'react-navigation'
 import { Provider } from 'react-redux'
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper'
 import Router from './router'
 import createStore from './common/stores'
 import { walletStorage } from './common/storages'
+import { OMGBackground } from './components/widgets'
 
 const App = () => {
   const [store, setStore] = useState(createStore({ wallets: [] }))
@@ -17,12 +20,33 @@ const App = () => {
     syncStoreWallets()
   }, [])
 
+  const theme = {
+    ...DefaultTheme,
+    dark: false,
+    roundness: 4,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: '#334e68',
+      accent: '#f1c40f',
+      background: '#f0f4f8',
+      surface: '#D9E2EC',
+      placeholder: '#BCCCDC',
+      input: '#FFFFFF'
+    }
+  }
+
   return (
     <Fragment>
-      <StatusBar barStyle='dark-content' />
-      <SafeAreaView style={styles.safeAreaView}>
+      <StatusBar barStyle='dark-content' backgroundColor='#f0f4f8' />
+      <SafeAreaView
+        style={styles.safeAreaView}
+        forceInset={{ top: 'never', bottom: 'never' }}>
         <Provider store={store}>
-          <Router />
+          <PaperProvider theme={theme}>
+            <OMGBackground style={styles.safeAreaView}>
+              <Router />
+            </OMGBackground>
+          </PaperProvider>
         </Provider>
       </SafeAreaView>
     </Fragment>
