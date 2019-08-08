fdb10d87e122cf55008109e4032292855d0421c8
euro <phuchit@omise.co>
2019-08-08 10:27:08 +0700

    Export new widgets

--- src/components/widgets/index.js
+++ src/components/widgets/index.js
@@ -2,11 +2,13 @@ import OMGAppBar from './omg-app-bar'
 import OMGBackground from './omg-background'
 import OMGBox from './omg-box'
 import OMGButton from './omg-button'
+import OMGCardAssets from './omg-card-assets'
 import OMGCardBalance from './omg-card-balance'
 import OMGEmpty from './omg-empty'
 import OMGMenu from './omg-menu'
 import OMGIcon from './omg-icon'
 import OMGItemWallet from './omg-item-wallet'
+import OMGItemToken from './omg-item-token'
 import OMGPasswordTextInput from './omg-password-text-input'
 import OMGRadioButton from './omg-radio-button'
 import OMGText from './omg-text'
@@ -22,10 +24,12 @@ export {
   OMGEmpty,
   OMGIcon,
   OMGItemWallet,
+  OMGItemToken,
   OMGPasswordTextInput,
   OMGRadioButton,
   OMGTextInput,
   OMGTitle,
   OMGText,
-  OMGMenu
+  OMGMenu,
+  OMGCardAssets
 }
