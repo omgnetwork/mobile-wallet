24e59248c5f31e3a64cb14b3b3394bf03f4fd7be
euro <phuchit@omise.co>
2019-08-12 10:39:37 +0700

    Add `react-native-config`

--- android/app/src/main/java/com/wallet/MainApplication.java
+++ android/app/src/main/java/com/wallet/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import br.com.classapp.RNSensitiveInfo.RNSensitiveInfoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
