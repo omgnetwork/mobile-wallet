d7d4d5fb366553866379be73bf911fa7e9796df1
euro <phuchit@omise.co>
2019-08-18 14:21:02 +0700

    Export more widgets

--- src/components/widgets/index.js
+++ src/components/widgets/index.js
@@ -1,3 +1,5 @@
+import OMGAddressInput from './omg-address-input'
+import OMGAmountInput from './omg-amount-input'
 import OMGAppBar from './omg-app-bar'
 import OMGAssetHeader from './omg-asset-header'
 import OMGAssetList from './omg-asset-list'
@@ -8,6 +10,7 @@ import OMGButton from './omg-button'
 import OMGEmpty from './omg-empty'
 import OMGMenu from './omg-menu'
 import OMGIcon from './omg-icon'
+import OMGImage from './omg-image'
 import OMGItemWallet from './omg-item-wallet'
 import OMGItemToken from './omg-item-token'
 import OMGQRCode from './omg-qr-code'
@@ -17,10 +20,14 @@ import OMGRadioButton from './omg-radio-button'
 import OMGTab from './omg-tab'
 import OMGText from './omg-text'
 import OMGTextInput from './omg-text-input'
+import OMGTokenInput from './omg-token-input'
 import OMGTitle from './omg-title'
 import OMGViewPager from './omg-view-pager'
+import OMGWalletAddress from './omg-wallet-address'
 
 export {
+  OMGAddressInput,
+  OMGAmountInput,
   OMGAppBar,
   OMGAssetHeader,
   OMGAssetList,
@@ -30,6 +37,7 @@ export {
   OMGButton,
   OMGEmpty,
   OMGIcon,
+  OMGImage,
   OMGItemWallet,
   OMGItemToken,
   OMGMenu,
@@ -39,7 +47,9 @@ export {
   OMGRadioButton,
   OMGTab,
   OMGTextInput,
+  OMGTokenInput,
   OMGTitle,
   OMGText,
-  OMGViewPager
+  OMGViewPager,
+  OMGWalletAddress
 }
