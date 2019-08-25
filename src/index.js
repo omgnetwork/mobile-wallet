0c6a90aa7ca94c28f2e083b1f735ee50407a7007
euro <phuchit@omise.co>
2019-08-25 13:36:13 +0700

    Fix error

--- src/index.js
+++ src/index.js
@@ -7,11 +7,10 @@ import createStore from 'common/stores'
 import { settingActions } from 'common/actions'
 import Config from 'react-native-config'
 import { notificationService } from 'common/services'
-import axios from 'axios'
 import { colors } from 'common/styles'
 import { PersistGate } from 'redux-persist/integration/react'
 
-YellowBox.ignoreWarnings(['Warning:', 'Setting'])
+YellowBox.ignoreWarnings(['Warning:', 'Setting', '`setBackgroundColor`'])
 const initialState = {
   wallets: [],
   setting: { provider: null, providerName: Config.ETHERSCAN_NETWORK }
