915fc05e3d16d518f661ecd5bc401caeb20ab8c3
okalouti <omar@omisego.co>
2019-10-25 17:23:33 +0700

    feat: adds arrow on right side

--- src/components/views/onboarding/Card.js
+++ src/components/views/onboarding/Card.js
@@ -2,28 +2,40 @@ import React from 'react'
 import { withTheme } from 'react-native-paper'
 import { withNavigation } from 'react-navigation'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import { OMGText } from '../../widgets'
+import { OMGIcon, OMGText } from '../../widgets'
 
 const Card = ({ navigation, theme, color, header, description }) => {
   return (
     <TouchableOpacity>
-      <View style={{ ...styles.container, backgroundColor: color }}>
-        <OMGText style={[styles.text(theme), styles.header]} weight='bold'>
-          {header}
-        </OMGText>
-        <OMGText style={[styles.text(theme), styles.subheader]}>
-          {description}
-        </OMGText>
+      <View>
+        <View style={{ ...styles.container, backgroundColor: color }}>
+          <View>
+            <OMGText style={[styles.text(theme), styles.header]} weight='bold'>
+              {header}
+            </OMGText>
+            <OMGText style={[styles.text(theme), styles.subheader]}>
+              {description}
+            </OMGText>
+          </View>
+          <OMGIcon name='chevron-right' size={28} style={styles.arrow(theme)} />
+        </View>
       </View>
     </TouchableOpacity>
   )
 }
 
 const styles = StyleSheet.create({
+  arrow: theme => ({
+    marginRight: 20,
+    color: theme.colors.white
+  }),
   container: {
-    justifyContent: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
     height: 100
   },
+
   text: theme => ({
     color: theme.colors.white,
     marginLeft: 20
