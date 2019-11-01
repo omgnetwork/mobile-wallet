b6ce727d70ff6690de9cd84814dcb4134e8457aa
okalouti <omar@omisego.co>
2019-11-01 16:36:45 +0700

    [ADD]: adds programmatic scroll functionality

--- src/components/widgets/omg-view-pager/index.js
+++ src/components/widgets/omg-view-pager/index.js
@@ -1,9 +1,18 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useCallback } from 'react'
 import { StyleSheet, ScrollView, Platform } from 'react-native'
 
-const OMGViewPager = ({ children, pageWidth }) => {
+const OMGViewPager = ({ children, pageWidth, currentPage }) => {
   const scrollView = useRef(null)
   const middlePageOffset = pageWidth - 16
+  const scroll = useRef(null)
+
+  const scrollTo = useCallback(
+    page => {
+      const targetPosition = (page - 1) * pageWidth - 16
+      scroll.current.scrollTo({ x: targetPosition, y: 0 })
+    },
+    [pageWidth]
+  )
 
   useEffect(() => {
     if (scrollView.current && Platform.OS === 'android') {
@@ -15,9 +24,14 @@ const OMGViewPager = ({ children, pageWidth }) => {
         })
       }, 300)
     }
-  }, [middlePageOffset])
+
+    setTimeout(() => {
+      scrollTo(currentPage)
+    }, 1)
+  }, [middlePageOffset, scrollTo, currentPage])
   return (
     <ScrollView
+      ref={scroll}
       contentContainerStyle={styles.container}
       contentOffset={{ x: middlePageOffset }}
       snapToAlignment='center'
