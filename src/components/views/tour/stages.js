617f3bc6c3cf292ebe9cafc77e11c975b66641b3
okalouti <omar@omisego.co>
2019-11-01 16:38:29 +0700

    [WIP]: adds tour steps

--- src/components/views/tour/stages.js
+++ src/components/views/tour/stages.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { withNavigation } from 'react-navigation'
+import { withNavigation, SwitchActions } from 'react-navigation'
 import { withTheme } from 'react-native-paper'
 import { StyleSheet, Dimensions } from 'react-native'
 
@@ -15,15 +15,31 @@ const Stages = ({
   stage,
   modalVisible,
   setModalVisible,
-  setSkipTour
+  setCurrentScrollPage,
+  setSkipTour,
+  setTourStageTracker
 }) => {
   const pageWidth = Dimensions.get('window').width
   const content = Tour.content[stage]
   const drawerNavigation = navigation.dangerouslyGetParent()
-  const rightButtonCallback = {
-    0: () => {
-      setModalVisible(false)
-      drawerNavigation.openDrawer()
+
+  const rightButtonCallback = () => {
+    setModalVisible(false)
+    setTourStageTracker(stage + 1)
+    switch (stage) {
+      case 0: {
+        drawerNavigation.openDrawer()
+        break
+      }
+      case 1: {
+        drawerNavigation.closeDrawer()
+        setCurrentScrollPage(3)
+        break
+      }
+      case 2: {
+        setCurrentScrollPage(2)
+        break
+      }
     }
   }
 
@@ -46,7 +62,7 @@ const Stages = ({
               rightButtonText={content.buttonTextRight}
               leftButtonText={content.buttonTextLeft}
               leftButtonCallback={leftButtonCallback[0]}
-              rightButtonCallback={rightButtonCallback[0]}
+              rightButtonCallback={rightButtonCallback}
               header={content.header}
               paragraphs={content.paragraphs}
             />
@@ -54,12 +70,90 @@ const Stages = ({
         />
       )
     }
+
+    case 1: {
+      return (
+        <OMGModal
+          type='triangle-up'
+          modalVisible={modalVisible}
+          height={250}
+          width={0.9 * pageWidth}
+          style={styles[stage]}
+          content={
+            <Tour.Screen
+              rightButtonText={content.buttonTextRight}
+              leftButtonText={content.buttonTextLeft}
+              leftButtonCallback={leftButtonCallback[stage]}
+              rightButtonCallback={rightButtonCallback}
+              header={content.header}
+              paragraphs={content.paragraphs}
+            />
+          }
+        />
+      )
+    }
+    case 2: {
+      return (
+        <OMGModal
+          type='triangle-up'
+          modalVisible={modalVisible}
+          height={220}
+          width={0.9 * pageWidth}
+          style={styles[stage]}
+          content={
+            <Tour.Screen
+              rightButtonText={content.buttonTextRight}
+              leftButtonText={content.buttonTextLeft}
+              leftButtonCallback={leftButtonCallback[stage]}
+              rightButtonCallback={rightButtonCallback}
+              header={content.header}
+              paragraphs={content.paragraphs}
+            />
+          }
+        />
+      )
+    }
+
+    case 3: {
+      return (
+        <OMGModal
+          type='triangle-up'
+          modalVisible={modalVisible}
+          height={250}
+          width={0.9 * pageWidth}
+          style={styles[stage]}
+          content={
+            <Tour.Screen
+              rightButtonText={content.buttonTextRight}
+              leftButtonText={content.buttonTextLeft}
+              leftButtonCallback={leftButtonCallback[stage]}
+              rightButtonCallback={rightButtonCallback}
+              header={content.header}
+              paragraphs={content.paragraphs}
+            />
+          }
+        />
+      )
+    }
+
+    default: {
+      return null
+    }
   }
 }
 
 const styles = StyleSheet.create({
   0: {
     marginTop: 310
+  },
+  1: {
+    marginTop: 180
+  },
+  2: {
+    marginTop: 540
+  },
+  3: {
+    marginTop: 220
   }
 })
 
@@ -68,6 +162,9 @@ const mapStateToProps = (state, ownProps) => ({})
 const mapDispatchToProps = (dispatch, ownProps) => ({
   setSkipTour: bool => {
     settingActions.setSkipTour(dispatch, bool)
+  },
+  setTourStageTracker: stage => {
+    settingActions.setTourStageTracker(dispatch, stage)
   }
 })
 
