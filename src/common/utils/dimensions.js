d31a0037c478399b94c4356a397ddc5572089a3c
euro <phuchit@omise.co>
2019-11-15 16:00:29 +0700

    Add dimension util

---
+++ src/common/utils/dimensions.js
@@ -0,0 +1,3 @@
+import { Dimensions } from 'react-native'
+export const windowWidth = Math.round(Dimensions.get('window').width)
+export const windowHeight = Math.round(Dimensions.get('window').height)
