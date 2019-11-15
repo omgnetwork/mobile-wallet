d9d09fde2cccbd7cd02810814e107a7455574a0a
euro <phuchit@omise.co>
2019-11-15 16:01:15 +0700

    Add main screen of the app useful for adding global popup

---
+++ src/components/views/main/index.js
@@ -0,0 +1,40 @@
+import React, { useCallback, useState } from 'react'
+import { View, StyleSheet } from 'react-native'
+import { withTheme } from 'react-native-paper'
+import { connect } from 'react-redux'
+import { onboardingActions } from 'common/actions'
+import { OnboardingTourGuide } from 'components/views'
+
+const MainContainer = ({ navigation, dispatchEnableOnboarding }) => {
+  const MainDrawerNavigator = navigation.getParam('navigator')
+
+  return (
+    <View style={styles.container}>
+      <MainDrawerNavigator navigation={navigation} />
+      <OnboardingTourGuide />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
+
+const mapStateToProps = (state, ownProps) => ({
+  primaryWallet: state.wallets.find(
+    w => w.address === state.setting.primaryWalletAddress
+  )
+})
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  dispatchEnableOnboarding: enabled => {
+    onboardingActions.setEnableOnboarding(dispatch, enabled)
+  }
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withTheme(MainContainer))
