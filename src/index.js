d98f26b9c64bf3212c8f7da4a64c7b82c1000b18
euro <phuchit@omise.co>
2019-11-15 16:02:14 +0700

    Simplify store initialization

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import { YellowBox } from 'react-native'
 import { Provider } from 'react-redux'
 import Router from 'router'
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper'
-import createStore from 'common/stores'
+import { store, persistor } from 'common/stores'
 import { settingActions } from 'common/actions'
 import Config from 'react-native-config'
 import { TransactionTracker } from 'common/tracker'
-import { OMGAlert, OMGInitializing } from 'components/widgets'
+import { OMGAlert } from 'components/widgets'
 import { notificationService } from 'common/services'
 import { colors } from 'common/styles'
 import { PersistGate } from 'redux-persist/integration/react'
@@ -15,27 +15,10 @@ import SplashScreen from 'react-native-splash-screen'
 
 YellowBox.ignoreWarnings(['Warning:', 'Setting', '`setBackgroundColor`'])
 
-const initialState = {
-  wallets: [],
-  setting: {
-    tourStage: 0,
-    provider: null,
-    providerName: Config.ETHERSCAN_NETWORK
-  }
-}
-const { store, persistor } = createStore(initialState)
-
 const App = () => {
   useEffect(() => {
-    function syncStorageToStore() {
-      const syncActions = [
-        settingActions.syncProviderToStore(Config.ETHERSCAN_NETWORK)
-      ]
-      syncActions.forEach(action => store.dispatch(action))
-      notificationService.init()
-    }
-
-    syncStorageToStore()
+    store.dispatch(settingActions.syncProviderToStore(Config.ETHERSCAN_NETWORK))
+    notificationService.init()
     SplashScreen.hide()
   }, [])
 
