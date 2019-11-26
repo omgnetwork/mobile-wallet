c8124de6dea5dc231b3538217933f5ec12a19d06
euro <phuchit@omise.co>
2019-11-26 13:46:05 +0700

    Setup everything

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['@babel/preset-env', 'module:metro-react-native-babel-preset'],
   env: {
     production: {
       // plugins: ['react-native-paper/babel']
-      plugins: ['transform-remove-console']
+      plugins: ['transform-remove-console', 'transform-es2015-modules-commonjs']
+    },
+    test: {
+      // plugins: ['react-native-paper/babel']
+      plugins: ['transform-es2015-modules-commonjs'],
+      presets: ['react-native', ['@babel/preset-env']]
+    },
+    development: {
+      plugins: ['transform-es2015-modules-commonjs']
     }
   },
   plugins: [
---
+++ jest/setup.js
@@ -0,0 +1,7 @@
+// jest.mock('PushNotificationIOS', () => {
+//   return {
+//     addEventListener: jest.fn(),
+//     requestPermissions: jest.fn(() => Promise.resolve()),
+//     getInitialNotification: jest.fn(() => Promise.resolve())
+//   }
+// })
