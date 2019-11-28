a90a79e45f348b24284a1544392cc918e22996f3
euro <phuchit@omise.co>
2019-11-28 12:23:14 +0700

    remove jest setup

--- jest/setup.js
+++
@@ -1,7 +0,0 @@
-// jest.mock('PushNotificationIOS', () => {
-//   return {
-//     addEventListener: jest.fn(),
-//     requestPermissions: jest.fn(() => Promise.resolve()),
-//     getInitialNotification: jest.fn(() => Promise.resolve())
-//   }
-// })
