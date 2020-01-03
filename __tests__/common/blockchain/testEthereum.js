79929b02a7459c66b42d8de6fd3b7c8df145e7dd
euro <phuchit@omisego.co>
2020-01-03 16:11:59 +0700

    Initialise ethereum boundary test

---
+++ __tests__/common/blockchain/testEthereum.js
@@ -0,0 +1,7 @@
+import { Ethereum } from 'common/ethereum'
+
+describe('Test Ethereum Boundary', () => {
+  it('importWalletMnemonic should return a wallet when given 12-words mnemonic', () => {
+    // Ethereum.importWal
+  })
+})
