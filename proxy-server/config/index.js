dea3517019e7a50d63cddc8d93c8a5997f632037
Unnawut Leepaisalsuwanna <unnawut@omisego.co>
2020-01-08 19:56:22 +0700

    Expose LOG_LEVEL through env var

--- proxy-server/config/index.js
+++ proxy-server/config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
   ETHEREUM_RPC_URL: process.env.ETHEREUM_RPC_URL || 'http://localhost:8545',
-  LOG_LEVEL: 'info',
+  LOG_LEVEL: process.env.LOG_LEVEL || 'info', // 'info' will log of all requests proxied
   DD_HOSTNAME: process.env.DD_HOSTNAME || 'localhost',
   DD_PORT: process.env.DD_PORT || 8125,
   RATE_LIMIT_WINDOW_MS: process.env.RATE_LIMIT_WINDOW_MS || 60 * 60 * 1000, // 60 minutes
