45eed52fb43144e61ee4d2f2306ab0003f9ce4bd
Unnawut Leepaisalsuwanna <unnawut@omisego.co>
2020-01-10 16:18:59 +0700

    Add test for service unavailable

--- proxy-server/test/handlers/ethereum-rpc-proxy.test.js
+++ proxy-server/test/handlers/ethereum-rpc-proxy.test.js
@@ -1,16 +1,14 @@
 const http = require('http')
 const express = require('express')
 const bodyParser = require('body-parser')
+
+const CONFIG = require('../../config')
 const ethereumRpcProxy = require('../../handlers/ethereum-rpc-proxy')
 
 var backend
 var proxy
 
 beforeAll(() => {
-  const backendApp = express()
-  backendApp.get('/', (req, res) => res.send('{"backend":"response"}'))
-  backend = backendApp.listen(8545)
-
   const proxyApp = express()
   proxyApp.use(bodyParser.json())
   proxyApp.use('/', ethereumRpcProxy())
@@ -19,15 +17,37 @@ beforeAll(() => {
 
 afterAll(() => {
   proxy && proxy.close()
-  backend && backend.close()
 })
 
-describe('ethereum-rpc-proxy', function() {
+describe('ethereum-rpc-proxy with backend available', function() {
+  beforeAll(() => {
+    const backendApp = express()
+    backendApp.get('/', (req, res) => res.send('{"backend":"response"}'))
+    backend = backendApp.listen(8545)
+  })
+
+  afterAll(() => {
+    backend && backend.close()
+  })
+
   it('should return a response from the backend', function(done) {
     http.get('http://localhost:3000', res => {
       res.on('data', chunk => {
         const responseBody = chunk.toString()
         expect(responseBody).toBe('{"backend":"response"}')
+        done()
+      })
+    })
+  })
+})
+
+describe('ethereum-rpc-proxy with backend unavailable', function() {
+  it('should return an error response', function(done) {
+    http.get('http://localhost:3000', res => {
+      res.on('data', chunk => {
+        const responseBody = chunk.toString()
+        const message = 'Error occured while trying to proxy to: ' + CONFIG.ETHEREUM_RPC_URL + '/'
+        expect(responseBody).toBe(message)
         done()
       })
     })
