7f24904ced581fb49ff3fdaf2fc6ba138fbc22b6
euro <phuchit@omisego.co>
2020-02-03 11:20:34 +0700

    Fix deposit color

--- src/components/views/deposit/index.js
+++ src/components/views/deposit/index.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react'
-import { View, StyleSheet, StatusBar } from 'react-native'
+import { View, StyleSheet, StatusBar, TouchableOpacity } from 'react-native'
 import { withTheme } from 'react-native-paper'
 import { SafeAreaView } from 'react-navigation'
 import { connect } from 'react-redux'
-import { OMGFontIcon, OMGBox, OMGText, OMGStatusBar } from 'components/widgets'
+import { OMGFontIcon, OMGText, OMGStatusBar } from 'components/widgets'
 
 const Deposit = ({ navigation, theme }) => {
   const ChildChainTransferNavigator = navigation.getParam('navigator')
@@ -22,25 +22,26 @@ const Deposit = ({ navigation, theme }) => {
   }, [navigation, theme.colors.gray4])
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={styles.container(theme)}>
       <OMGStatusBar
         barStyle={'light-content'}
         backgroundColor={theme.colors.white}
       />
       <View style={styles.titleContainer}>
-        <OMGText style={styles.title(theme)}>Deposit</OMGText>
-        <OMGBox
+        <OMGText style={styles.title(theme)} weight='regular'>
+          Deposit
+        </OMGText>
+        <TouchableOpacity
           onPress={() => {
             navigation.navigate('Balance')
-          }}
-          style={styles.iconBox}>
+          }}>
           <OMGFontIcon
             name='x-mark'
             size={18}
-            color={theme.colors.gray3}
+            color={theme.colors.white}
             style={styles.icon}
           />
-        </OMGBox>
+        </TouchableOpacity>
       </View>
       <ChildChainTransferNavigator navigation={navigation} />
     </SafeAreaView>
@@ -48,10 +49,13 @@ const Deposit = ({ navigation, theme }) => {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
+  container: theme => ({
+    flex: 1,
+    backgroundColor: theme.colors.gray4
+  }),
   titleContainer: {
+    paddingVertical: 24,
+    paddingRight: 16,
     flexDirection: 'row',
     alignItems: 'center'
   },
@@ -60,7 +64,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 16,
     fontSize: 18,
     textTransform: 'uppercase',
-    color: theme.colors.gray3
+    color: theme.colors.white
   }),
   iconBox: {
     padding: 16
