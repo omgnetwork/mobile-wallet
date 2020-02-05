f52f3233a968b45525685cca19de9671e055a4fa
euro <phuchit@omisego.co>
2020-02-05 16:44:59 +0700

    Fix bug rendering token icon

--- src/components/widgets/omg-token-icon/index.js
+++ src/components/widgets/omg-token-icon/index.js
@@ -1,24 +1,42 @@
 import React, { useState, useEffect } from 'react'
 import { Image, View } from 'react-native'
 import { ContractAddress } from 'common/constants'
-import { OMGIdenticon } from 'components/widgets'
+import { OMGIdenticon, OMGEmpty } from 'components/widgets'
 import { withTheme } from 'react-native-paper'
 import { Token } from 'common/blockchain'
 
 const OMGTokenIcon = ({ token, theme, style, size }) => {
   const [isError, setIsError] = useState(false)
-  const contractAddressChecksum = Token.getContractAddressChecksum(
-    token.contractAddress
+  const [isEth, setIsEth] = useState(
+    token.contractAddress === ContractAddress.ETH_ADDRESS
   )
-  const identiconUri = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${contractAddressChecksum}/logo.png`
-  const ethUri = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/info/logo.png`
-  const [iconUri, setIconUri] = useState(identiconUri)
+  const [contractAddressChecksum, setContractAddressChecksum] = useState(null)
+  const [iconUri, setIconUri] = useState(null)
 
   useEffect(() => {
-    const isEth = token.contractAddress === ContractAddress.ETH_ADDRESS
-    const uri = isEth ? ethUri : identiconUri
+    const erc20Uri = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${contractAddressChecksum}/logo.png`
+    const ethUri = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/info/logo.png`
+    const uri = isEth ? ethUri : erc20Uri
     setIconUri(uri)
-  }, [contractAddressChecksum, ethUri, identiconUri, token.contractAddress])
+  }, [token, isEth, contractAddressChecksum])
+
+  useEffect(() => {
+    const checksum = Token.getContractAddressChecksum(token.contractAddress)
+    setContractAddressChecksum(checksum)
+    if (token.contractAddress === ContractAddress.ETH_ADDRESS) {
+      setIsEth(true)
+      setIsError(false)
+    } else {
+      setIsEth(false)
+    }
+  }, [contractAddressChecksum, token])
+
+  if (!contractAddressChecksum) {
+    return <OMGEmpty loading={true} />
+  }
+
+  console.log(token.tokenSymbol, isError, iconUri)
+
   return isError ? (
     <OMGIdenticon
       hash={contractAddressChecksum}
@@ -32,7 +50,10 @@ const OMGTokenIcon = ({ token, theme, style, size }) => {
         source={{
           uri: iconUri
         }}
-        onError={() => setIsError(true)}
+        onLoad={() => setIsError(false)}
+        onError={() => {
+          if (!isEth) setIsError(true)
+        }}
       />
     </View>
   )
