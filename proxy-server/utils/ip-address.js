ac520c15081980d917936fb1a69338f717abc1fe
euro <phuchit@omisego.co>
2020-03-16 13:54:54 +0700

    handle when X-Forwarded-For is undefined

--- proxy-server/utils/ip-address.js
+++ proxy-server/utils/ip-address.js
@@ -1,7 +1,8 @@
 const getOriginalRequestIPFromCloudFlare = req => {
   return (
     req.headers['CF-Connecting-IP'] ||
-    req.headers['X-Forwarded-For'].split(',')[0]
+    (req.headers['X-Forwarded-For'] &&
+      req.headers['X-Forwarded-For'].split(',')[0])
   )
 }
 
