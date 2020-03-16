b321199eefc62f93a15f92eea56f257b40104d60
euro <phuchit@omisego.co>
2020-03-16 11:27:54 +0700

    Change keyGenerator function name

--- proxy-server/utils/rate-limiter.js
+++ proxy-server/utils/rate-limiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit')
 const ipAddress = require('./ip-address')
 const CONFIG = require('../config')
 
-const keyGenerator = req => {
+const uniqueRequesterIdentifier = req => {
   return ipAddress.getOriginalRequestIPFromCloudFlare(req) || req.ip
 }
 
@@ -10,7 +10,7 @@ const rateLimiter = () =>
   rateLimit({
     windowMs: CONFIG.RATE_LIMIT_WINDOW_MS,
     max: CONFIG.RATE_LIMIT,
-    keyGenerator
+    keyGenerator: uniqueRequesterIdentifier
   })
 
 module.exports = rateLimiter
