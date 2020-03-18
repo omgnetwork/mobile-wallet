70a803cd2fe582382108ba91316e50dff4ac9f29
Phuchit Sirimongkolsathien <phuchit@omisego.co>
2020-03-18 12:52:15 +0700

    Fix and improve mwproxy logs (#147)
    * hide url in log
    
    * Fix wrong header key
    
    * Fix lint

--- proxy-server/utils/logger.js
+++ proxy-server/utils/logger.js
@@ -1,10 +1,22 @@
 const winston = require('winston')
 const CONFIG = require('../config')
 
+const urlRegex = /(\(.*?)?\b((?:http(s)?):\/\/[-a-z0-9+&@#/%?=~_()|!:,.;]*[-a-z0-9+&@#/%=~_()|])/gi
+
+// Need to be put before winston.format.json() in the winston.format.combine function.
+const hideUrlFormat = winston.format(info => {
+  const { message } = info
+  return {
+    ...info,
+    message: message.replace(urlRegex, '<redacted>')
+  }
+})
+
 const logger = winston.createLogger({
   level: CONFIG.LOG_LEVEL,
   format: winston.format.combine(
     winston.format.timestamp(),
+    hideUrlFormat(),
     winston.format.json()
   ),
   transports: [new winston.transports.Console()]
