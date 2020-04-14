71ffc32eaa9c71a70f31b077db0a21e1ac28fa01
euro <phuchit@omisego.co>
2020-04-14 15:06:05 +0700

    Fix typo

--- src/common/clients/childchain.js
+++ src/common/clients/childchain.js
@@ -3,5 +3,5 @@ import { ChildChain } from '@omisego/react-native-omg-js'
 // Assume Config.CHILDCHAIN_WATCHER_URL is always has '/' at the end.
 export default watcherUrl =>
   new ChildChain({
-    wathcerUrl: watcherUrl.endsWith('/') ? watcherUrl : `${watcherUrl}/`
+    watcherUrl: watcherUrl.endsWith('/') ? watcherUrl : `${watcherUrl}/`
   })
