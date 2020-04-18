b7cf77f955c684b4424c9e73363f63cf97c76ffa
euro <phuchit@omisego.co>
2020-04-18 13:48:54 +0700

    Refactor styles utils

--- src/common/utils/styles.js
+++ src/common/utils/styles.js
@@ -1,10 +1,11 @@
 import { Dimensions } from 'react-native'
 
 export const getResponsiveSize = (defaultSize, { small, medium }) => {
-  const ratio = getScreenRatio()
-  if (getAspectRatio() > 2 || ratio > 3) {
+  const widthRatio = getWidthRatio()
+  const aspectRatio = getAspectRatio()
+  if (aspectRatio > 2 || widthRatio > 3) {
     return defaultSize
-  } else if (ratio > 2) {
+  } else if (widthRatio > 2) {
     return medium
   } else {
     return small
@@ -12,21 +13,22 @@ export const getResponsiveSize = (defaultSize, { small, medium }) => {
 }
 
 export const combine = (defaultStyle, { small, medium }) => {
-  const ratio = getScreenRatio()
-  if (getAspectRatio() > 2 || ratio >= 3) {
+  const widthRatio = getWidthRatio()
+  const aspectRatio = getAspectRatio()
+  if (aspectRatio > 2 || widthRatio >= 3) {
     return defaultStyle
-  } else if (ratio > 2) {
+  } else if (widthRatio > 2) {
     return merge(defaultStyle, medium)
   } else {
     return merge(defaultStyle, small)
   }
 }
 
-export const getScreenRatio = () => {
+const getWidthRatio = () => {
   return Dimensions.get('window').width / 160
 }
 
-export const getAspectRatio = () => {
+const getAspectRatio = () => {
   return Dimensions.get('window').height / Dimensions.get('window').width
 }
 
