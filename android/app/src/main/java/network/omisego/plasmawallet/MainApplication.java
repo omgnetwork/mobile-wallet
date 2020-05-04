62830bcb811034e87e4e19d1b76a6da0bae90334
euro <phuchit@omisego.co>
2020-05-04 16:21:26 +0700

    Apply changes following https://react-native-community.github.io/upgrade-helper/?from=0.61.5&to=0.62.2

--- android/app/src/main/java/network/omisego/plasmawallet/MainApplication.java
+++ android/app/src/main/java/network/omisego/plasmawallet/MainApplication.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import io.intercom.android.sdk.Intercom;
@@ -47,24 +48,27 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    initializeFlipper(this); // Remove this line if you don't want Flipper enabled
+    initializeFlipper(this, getReactNativeHost().getReactInstanceManager()); // Remove this line if you don't want Flipper enabled
     Intercom.initialize(this, "android_sdk-a280efeb2688e63a8515ac6fc15912f25015e6bf", "r0u30zii");
   }
 
   /**
    * Loads Flipper in React Native templates.
+   * Call this in the onCreate method with something like
+   * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
    *
    * @param context
+   * @param reactInstanceManager
    */
-  private static void initializeFlipper(Context context) {
+  private static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
     if (BuildConfig.DEBUG) {
       try {
         /*
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
-        aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
+        Class<?> aClass = Class.forName("network.omisego.plasmawallet.ReactNativeFlipper");
+        aClass.getMethod("initializeFlipper", Context.class, ReactInstanceManager.class).invoke(null, context, reactInstanceManager);
       } catch (ClassNotFoundException e) {
         e.printStackTrace();
       } catch (NoSuchMethodException e) {
