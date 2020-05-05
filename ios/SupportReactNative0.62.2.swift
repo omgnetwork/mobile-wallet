0bb949543ea6fdfb363eb08b6e793221996c8676
euro <phuchit@omisego.co>
2020-05-05 12:44:01 +0700

    Update react-navigation version to 4.3.9

--- ios/SupportReactNative0.62.2.swift
+++ ios/SupportReactNative0.62.2.swift
@@ -1,9 +1,12 @@
-N//
-//  SupportReact.swift
+//
+//  SupportReactNative0.62.2.swift
 //  PlasmaWallet
 //
 //  Created by Phuchit Sirimongkolsathien on 5/5/2563 BE.
-//  Copyright © 2563 OmiseGO. All rights reserved.
+//  Copyright © 2020 OmiseGO. All rights reserved.
 //
+
+// This empty file needed to be added for support ReactNative version 0.62.2
+// Reference: https://github.com/facebook/react-native/issues/28406#issuecomment-604792832
 
 import Foundation
