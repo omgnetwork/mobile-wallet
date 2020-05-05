684d037e4954774ff604fe80f682553b53abecbd
euro <phuchit@omisego.co>
2020-05-05 12:26:23 +0700

    Fix compile issues

---
+++ ios/SupportReactNative0.62.2.swift
@@ -0,0 +1,9 @@
+N//
+//  SupportReact.swift
+//  PlasmaWallet
+//
+//  Created by Phuchit Sirimongkolsathien on 5/5/2563 BE.
+//  Copyright © 2563 OmiseGO. All rights reserved.
+//
+
+import Foundation
