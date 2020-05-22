1afe705036ba8c5d7db4a4607fc48aa826c44786
euro <phuchit@omisego.co>
2020-05-22 17:59:12 +0700

    update childchain initialization

--- src/common/clients/index.js
+++ src/common/clients/index.js
@@ -1,10 +1,11 @@
-import Config from 'react-native-config'
+:import Config from 'react-native-config'
 import createWeb3 from './web3'
 import createRootchain from './rootchain'
 import createChildchain from './childchain'
 
 const web3HttpProvider = Config.WEB3_HTTP_PROVIDER
 const plasmaContractAddress = Config.PLASMA_FRAMEWORK_CONTRACT_ADDRESS
+const watcherURL = Config.WATCHER_URL
 
 // Assume Config.WATCHER_URL is always has '/' at the end.
 const watcherUrl = Config.WATCHER_URL.slice(0, -1)
@@ -13,5 +14,5 @@ export const web3 = createWeb3(web3HttpProvider)
 
 export const Plasma = {
   RootChain: createRootchain(web3, plasmaContractAddress),
-  ChildChain: createChildchain(watcherUrl)
+  ChildChain: createChildchain(watcherURL, plasmaContractAddress)
 }
