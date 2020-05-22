15f6ea8d48c960372a1af7b917a79eaf82dad8a6
euro <phuchit@omisego.co>
2020-05-22 15:54:06 +0700

    Make sure url does not ended with '/'

--- src/common/clients/childchain.js
+++ src/common/clients/childchain.js
@@ -1,7 +1,6 @@
 import { ChildChain } from '@omisego/react-native-omg-js'
 
-// Assume Config.WATCHER_URL is always has '/' at the end.
 export default watcherUrl =>
   new ChildChain({
-    watcherUrl: watcherUrl.endsWith('/') ? watcherUrl : `${watcherUrl}/`
+    watcherUrl: watcherUrl.endsWith('/') ? watcherUrl.slice(0, -1) : watcherUrl
   })
