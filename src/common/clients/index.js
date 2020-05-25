68f3f20f2c6a757b18db74e1c6f0d3c34acf6ba1
Phuchit Sirimongkolsathien <phuchit@omisego.co>
2020-05-25 14:32:28 +0700

    Fix transfer error (#203)
    * Increase decimals visibility
    
    * Make sure url does not ended with '/'
    
    * Get all utxos instead specific token when transfer

--- src/common/clients/index.js
+++ src/common/clients/index.js
@@ -7,11 +7,9 @@ const web3HttpProvider = Config.WEB3_HTTP_PROVIDER
 const plasmaContractAddress = Config.PLASMA_FRAMEWORK_CONTRACT_ADDRESS
 
 // Assume Config.WATCHER_URL is always has '/' at the end.
-const watcherUrl = Config.WATCHER_URL.slice(0, -1)
-
 export const web3 = createWeb3(web3HttpProvider)
 
 export const Plasma = {
   RootChain: createRootchain(web3, plasmaContractAddress),
-  ChildChain: createChildchain(watcherUrl)
+  ChildChain: createChildchain(Config.WATCHER_URL)
 }
