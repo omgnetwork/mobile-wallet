833dd46dd4f4a130bfb74741691d56f725768b36
okalouti <omar@omisego.co>
2020-06-05 14:18:04 +0200

    re-instate eslintrc.js and remove commented rules

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  overrides: [
+    {
+      files: ['**/tests/**/*.js']
+    }
+  ],
+  plugins: ['react', 'react-native'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    'react/prop-types': 'off',
+    'no-unused-vars': [2, { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }]
+  },
+  env: {
+    jest: true,
+    node: true,
+    es6: true
+  },
+
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  }
+}
