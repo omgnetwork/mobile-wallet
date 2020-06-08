6f56f414a4a55fc3797aebbb190cafde289da365
okalouti <omar@omisego.co>
2020-06-08 10:00:48 +0200

    lint-fix: use of `hasOwnProperty`

--- __mocks__/@react-native-community/async-storage/index.js
+++ __mocks__/@react-native-community/async-storage/index.js
@@ -9,12 +9,12 @@ export default {
   },
   getItem: key => {
     return new Promise(resolve => {
-      return cache.hasOwnProperty(key) ? resolve(cache[key]) : resolve(null)
+      return key in cache? resolve(cache[key]) : resolve(null)
     })
   },
   removeItem: key => {
     return new Promise((resolve, reject) => {
-      return cache.hasOwnProperty(key)
+      return  key in cache
         ? resolve(delete cache[key])
         : reject('No such key!')
     })
