4582f50ace65430de19971094455e58c601bff2f
euro <phuchit@omisego.co>
2020-06-10 17:00:01 +0700

    Fix exit flow

--- src/navigator/exit/index.js
+++ src/navigator/exit/index.js
@@ -5,7 +5,6 @@ export default createStackNavigator(
   {
     ExitSelectToken: Views.ExitSelectToken,
     ExitWarning: Views.ExitWarning,
-    ExitSelectBalance: Views.ExitSelectBalance,
     ExitSelectUtxo: Views.ExitSelectUtxo,
     ExitSelectFee: Views.ExitSelectFee,
     ExitForm: Views.ExitForm
