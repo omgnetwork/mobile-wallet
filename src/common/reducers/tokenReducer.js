9d6bd416773d3eb15f63b14b3cf040ba095c2000
okalouti <omar@omisego.co>
2020-07-11 09:17:07 +0200

    remove console.log

--- src/common/reducers/tokenReducer.js
+++ src/common/reducers/tokenReducer.js
@@ -1,7 +1,6 @@
 export const tokenReducer = (state = {}, action) => {
   switch (action.type) {
     case 'TOKEN/UPDATE_CACHE':
-      console.log(action.data)
       return {
         ...state,
         ...action.data
