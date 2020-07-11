096616191616ba8984108e81fd94a55d29701074
euro <phuchit@omisego.co>
2020-07-11 15:43:35 +0700

    Implement new agreement screen

--- src/components/views/agreement/index.js
+++ src/components/views/agreement/index.js
@@ -4,7 +4,9 @@ import { withTheme } from 'react-native-paper'
 import { withNavigation, SafeAreaView } from 'react-navigation'
 import { OMGText, OMGButton, OMGStatusBar } from 'components/widgets'
 import { OnboardingAgreement } from './assets'
-import { Styles } from 'common/utils'
+import { Styles, Dimensions } from 'common/utils'
+
+const { windowHeight } = Dimensions
 
 const Agreement = ({ navigation, theme }) => {
   const destination = navigation.getParam('destination')
@@ -24,45 +26,46 @@ const Agreement = ({ navigation, theme }) => {
         barStyle={'light-content'}
         backgroundColor={theme.colors.black5}
       />
+      <View style={styles.headerContainer}>
+        <OnboardingAgreement
+          style={styles.image}
+          width={windowHeight * 0.15}
+          height={windowHeight * 0.15}
+        />
+        <OMGText style={styles.headerText} weight='regular'>
+          Before you begin, let’s make sure we’re on the same page.
+        </OMGText>
+      </View>
       <ScrollView
-        contentContainerStyle={styles.contentContainer}
-        showsVerticalScrollIndicator={false}>
-        <View style={styles.headerContainer}>
-          <OnboardingAgreement
-            width={Styles.getResponsiveSize(140, { small: 99, medium: 120 })}
-            height={Styles.getResponsiveSize(116, { small: 83, medium: 99 })}
-            style={styles.image}
-          />
-          <OMGText style={styles.headerText} weight='regular'>
-            Nice choice! But before you start, let’s make sure we’re on the same
-            page :)
+        contentContainerStyle={styles.scrollContainer}
+        indicatorStyle='white'>
+        <>
+          <OMGText style={styles.contentText1} weight='regular'>
+            This wallet is your first official gateway to OmiseGO Network.
           </OMGText>
-        </View>
-        <OMGText style={styles.contentText1}>
-          This wallet is your first official gateway to OmiseGO Network.
-        </OMGText>
-        <OMGText style={styles.contentText2}>
-          Get first-hand experience of how the Plasma Layer-2 solution works.
-          You can deposit and transfer any ERC-20 compliant token. Transactions
-          will incur charges and fees that are collected in OMG.
-        </OMGText>
-        <View style={styles.buttonContainer}>
-          <OMGButton
-            style={styles.confirmButton}
-            textStyle={styles.confirmButtonText}
-            textweight='regular'
-            onPress={handleAcceptPressed}>
-            I UNDERSTAND AND ACCEPT
-          </OMGButton>
-          <OMGButton
-            style={styles.declineButton}
-            textStyle={styles.declineButtonText}
-            onPress={handleDeclinePressed}
-            textweight='regular'>
-            DECLINE
-          </OMGButton>
-        </View>
+          <OMGText style={styles.contentText2} weight='regular'>
+            Get first-hand experience of how the Plasma Layer-2 solution works.
+            You can deposit and transfer any ERC-20 compliant token.
+            Transactions will incur charges and fees that are collected in OMG.
+          </OMGText>
+        </>
       </ScrollView>
+      <View style={styles.buttonContainer}>
+        <OMGButton
+          style={styles.confirmButton}
+          textStyle={styles.confirmButtonText}
+          textweight='regular'
+          onPress={handleAcceptPressed}>
+          I UNDERSTAND AND ACCEPT
+        </OMGButton>
+        <OMGButton
+          style={styles.declineButton}
+          textStyle={styles.declineButtonText}
+          onPress={handleDeclinePressed}
+          textweight='regular'>
+          DECLINE
+        </OMGButton>
+      </View>
     </SafeAreaView>
   )
 }
@@ -79,26 +82,26 @@ const createStyles = theme =>
       marginTop: Styles.getResponsiveSize(24, { small: 16, medium: 16 })
     },
     headerContainer: {
-      backgroundColor: theme.colors.black5
+      alignItems: 'center'
     },
     headerText: {
       color: theme.colors.white,
-      fontSize: Styles.getResponsiveSize(30, { small: 20, medium: 28 }),
-      marginTop: Styles.getResponsiveSize(24, { small: 12, medium: 20 })
+      textAlign: 'center',
+      opacity: 0.8,
+      fontSize: Styles.getResponsiveSize(28, { small: 20, medium: 24 }),
+      marginTop: Styles.getResponsiveSize(20, { small: 12, medium: 16 })
     },
-    contentContainer: {
+    scrollContainer: {
       flexGrow: 1,
-      backgroundColor: theme.colors.black5,
-      paddingVertical: Styles.getResponsiveSize(24, { small: 8, medium: 16 })
+      flexDirection: 'column',
+      paddingRight: 16,
+      marginVertical: 16
     },
     buttonContainer: {
-      flex: 1,
       justifyContent: 'flex-end',
-      marginTop: 16,
-      backgroundColor: theme.colors.black5
+      paddingVertical: 16
     },
     contentText1: {
-      marginTop: Styles.getResponsiveSize(16, { small: 8, medium: 12 }),
       color: theme.colors.gray6,
       fontSize: Styles.getResponsiveSize(18, { small: 14, medium: 16 }),
       lineHeight: Styles.getResponsiveSize(28, { small: 20, medium: 24 })
