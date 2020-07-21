53404e6835b7eb1aacbcb0be73fc92bd13fa0c44
okalouti <omar@omisego.co>
2020-07-21 11:15:32 +0700

    feat: ethereum address check

--- validateEnv.js
+++ validateEnv.js
@@ -1,3 +1,4 @@
+const axios = require('axios')
 const _ = require('lodash')
 require('dotenv').config()
 
@@ -13,7 +14,6 @@ const urlsCorrectlyPrefixed = (
   ]
 ) => {
   for (const url of urlsToVerify) {
-    console.log(url, env[url])
     if (!_.startsWith(env[url], 'https://')) {
       console.error(`${url} must start with 'https://'`)
       return false
@@ -22,8 +22,55 @@ const urlsCorrectlyPrefixed = (
   return true
 }
 
+const getWatcherEthereumContractAddresses = async (env = process.env) => {
+  try {
+    const watcherConfig = await axios.post(env.WATCHER_URL + '/status.get', {})
+    return watcherConfig['data']['data']['contract_addr']
+  } catch {
+    console.error('Could not connect to given Watcher URL.')
+    return null
+  }
+}
+
+const addressWarn = addressType => {
+  console.warn(
+    `Given ${addressType} contract address does not match given Watcher configuration`
+  )
+}
+
+const correctEthereumAddresses = async (env = process.env) => {
+  const watcherConfig = await getWatcherEthereumContractAddresses()
+
+  if (watcherConfig['erc20_vault'] !== env.ERC20_VAULT_CONTRACT_ADDRESS) {
+    addressWarn('ERC20')
+    return false
+  }
+
+  if (watcherConfig['eth_vault'] !== env.ETH_VAULT_CONTRACT_ADDRESS) {
+    addressWarn('ETH')
+    return false
+  }
+
+  if (
+    watcherConfig['payment_exit_game'] !==
+    env.PLASMA_PAYMENT_EXIT_GAME_CONTRACT_ADDRESS
+  ) {
+    addressWarn('EXIT GAME')
+    return false
+  }
+
+  if (
+    watcherConfig['plasma_framework'] !== env.PLASMA_FRAMEWORK_CONTRACT_ADDRESS
+  ) {
+    addressWarn('PLASMA_FRAMEWORK')
+    return false
+  }
+
+  return true
+}
+
 const runEnvironmentVariableChecks = async (
-  checks = [urlsCorrectlyPrefixed]
+  checks = [urlsCorrectlyPrefixed, correctEthereumAddresses]
 ) => {
   for (const check of checks) {
     const pass = await check()
